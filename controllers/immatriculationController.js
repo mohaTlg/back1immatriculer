13c66f28449639259b09f1feb4442cd11320c7f8
Mamadou Moussa Diallo <mamadoumoussa.diallo.9212@gmail.com>
2022-04-15 15:48:54 +0000

    algorithme d'imatriculation complet

--- controllers/immatriculationController.js
+++ controllers/immatriculationController.js
@@ -1,15 +1,18 @@
 const ImmatriculationModel = require("../models/immatriculationModel");
 
-module.exports.createImmatriculation = async (req, res) => {
+module.exports.createImmatriculationVA = async (req, res) => {
   //console.log(db.immatriculations.stats());
-  let numcg = 0;
   let numIm = 0;
   let seriecg = "A";
+  let numcg = 0;
 
   const allImm = await ImmatriculationModel.find().select(); // a revoir en fonction du mode d'exploitation
+
+  const vaImm = await ImmatriculationModel.find({ categorie: "VA" }).select();
+
   numcg = allImm.length + 1;
 
-  numIm = allImm.length + 1;
+  numIm = vaImm.length + 1;
   //numIm = 100099;
 
   if (numIm > 19998) {
@@ -25,6 +28,49 @@ module.exports.createImmatriculation = async (req, res) => {
     numero_carte_grise: numcg,
     serie_carte_grise: seriecg,
     categorie: "VA",
+    //$inc: { numero_carte_grise: 1 },
+  };
+
+  try {
+    const immatriculation = await ImmatriculationModel.create(immatricul);
+    return res.status(201).json({
+      status: "success",
+      message: "immatriculation faite avec succes",
+      immatriculation,
+    });
+  } catch (err) {
+    return res.status(400).send({ status: "error", message: err });
+  }
+};
+
+module.exports.createImmatriculationEP = async (req, res) => {
+  //console.log(db.immatriculations.stats());
+  let numIm = 0;
+  let seriecg = "A";
+  let numcg = 0;
+
+  const allImm = await ImmatriculationModel.find().select(); // a revoir en fonction du mode d'exploitation
+
+  const epImm = await ImmatriculationModel.find({ categorie: "EP" }).select();
+
+  numcg = allImm.length + 1;
+
+  numIm = epImm.length + 1;
+  //numIm = 10003;
+
+  if (numIm > 19998) {
+    numIm = numIm - 19998;
+    seriecg = "C";
+  } else if (numIm > 9999) {
+    numIm = numIm - 9999;
+    seriecg = "B";
+  }
+
+  const immatricul = {
+    numero_immatriculation: numIm,
+    numero_carte_grise: numcg,
+    serie_carte_grise: seriecg,
+    categorie: "EP",
     //$inc: { numero_carte_grise: 1 },
   };
 
--- routes/immatriculationRoutes.js
+++ routes/immatriculationRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const immatriculationController = require("../controllers/immatriculationController");
 
-router.post("/", immatriculationController.createImmatriculation);
+router.post("/va", immatriculationController.createImmatriculationVA);
+router.post("/ep", immatriculationController.createImmatriculationEP);
 
 module.exports = router;
